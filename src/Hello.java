8d0fedcb0863b8caec232132743374fb325d560d
bforman <bforman@users.noreply.github.com>
2016-01-30 18:20:48 -0500

    Added features to: name program (CPU name), ensure proper case for names, make message conversational

--- src/Hello.java
+++ src/Hello.java
@@ -2,12 +2,32 @@
 public class Hello {
 	public static void main(String[] args) {
 		String name;
+		String cpuName;
 		if (args.length == 0) {
 			System.out.println("Hello Ben");
 		}
 		else {
-			name = args[0];
-			System.out.println("Hello " + name);
+			if (args.length == 3) {
+				//System.out.println("in");
+				//System.out.println(args[0]);
+				if (args[0].equals("-me"))
+				{
+					cpuName = args[1];
+					int len = cpuName.length();
+					cpuName = cpuName.substring(0,1).toUpperCase() + cpuName.substring (1,len).toLowerCase();
+					name = args[2];
+					int l = name.length();
+					name = name.substring(0,1).toUpperCase() + name.substring (1,l).toLowerCase();
+					System.out.println("Hello " + name + ". My name is " + cpuName + ".");
+					System.out.println("How are you " + name + "?");
+				}
+			}
+			else if (args.length == 1) {
+				name = args[0];
+				int len = name.length();
+				name = name.substring(0,1).toUpperCase() + name.substring (1,len).toLowerCase();
+				System.out.println("Hello " + name);
+			}
 		}
 	}
 }
